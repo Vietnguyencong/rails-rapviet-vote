1b20638fd35b5c8fa4df5ee58e5f463485711476
tnle379 <ntle18@uh.edu>
2020-10-31 09:45:06 -0500

    convert data for chartkick

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,12 +4,23 @@ class HomeController < ApplicationController
     @data = get_rapper_data
   end
 
+  #get data for chartkick
   def get_rapper_data
     _vote_count = {}
     Rapper.all.each do |r|
       _vote_count.merge!(r.name => r.get_upvotes.size)
     end
-    return _vote_count.sort_by(&:first)
-    
+
+    return map_objects(_vote_count.sort_by(&:first))
   end
+
+  # convert hash data into nested hash format for chartkick
+  def map_objects(hash)
+    new_obj = []
+    hash.group_by(&:shift).each do |key,val|
+      new_obj << {"name" => key, "data" => {"" => val}}
+    end
+    return new_obj
+  end
+
 end
