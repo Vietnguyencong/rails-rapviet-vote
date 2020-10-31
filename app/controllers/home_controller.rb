920cfaa3c8a22b924192188dfc495bd0001529d8
Vietnguyencong <vietnguyencong99@gmail.com>
2020-10-31 01:30:37 -0500

    modify the view index home, change the model for vote, add confirm when vote, add view after the user do the vote

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,7 +7,7 @@ class HomeController < ApplicationController
   def get_rapper_data
     _vote_count = {}
     Rapper.all.each do |r|
-      _vote_count.merge!(r.name => r.get_upvotes.size)
+    _vote_count.merge!(r.name => r.get_upvotes.size)
     end
     return _vote_count.sort_by(&:first)
     
