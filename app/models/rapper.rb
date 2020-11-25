b6152edf968978d83f5cf37ae660e5d06237227c
tnle379 <ntle18@uh.edu>
2020-11-24 19:53:54 -0600

    edit card background scss

--- app/models/rapper.rb
+++ app/models/rapper.rb
@@ -1,5 +1,5 @@
 class Rapper < ApplicationRecord
     acts_as_votable
-    has_many_attached :photos, dependent: :destroy
+    has_many_attached :photos
     validates :photos, content_type: [:png, :jpg, :jpeg]
 end
