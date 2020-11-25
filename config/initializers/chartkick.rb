13bea3c3d9571b5cc8fe08d77e9f8f41242e42c3
tnle379 <ntle18@uh.edu>
2020-11-25 00:59:12 -0600

    add information for charts

--- config/initializers/chartkick.rb
+++ config/initializers/chartkick.rb
@@ -1,5 +1,5 @@
 Chartkick.options = {
-  colors: ["#63b598","#ce7d78" , "#ea9e70", "#a48a9e", "#c6e1e8"]
+  colors: ["#63b598","#ce7d78" , "#ea9e70", "#a48a9e", "#c6e1e8", "#dbd21a"]
 }
 
 # Chartkick.options[:content_for] = :charts_js
